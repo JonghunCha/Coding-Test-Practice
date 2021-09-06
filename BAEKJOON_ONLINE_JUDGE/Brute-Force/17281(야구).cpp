e62643f43f6dc2fa2873d889bff7eba010309522
JonghunCha <jonghun0210.bb@gmail.com>
2021-09-06 19:04:47 +0900

    commit 17281.cpp

--- BAEKJOON_ONLINE_JUDGE/Brute-Force/17281(야구).cpp
+++ BAEKJOON_ONLINE_JUDGE/Brute-Force/17281(야구).cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int answer = 0;
 
+void Make_Combination(const int N, const int inputs[51][10], int combination[10], int index);
 void Get_Answer(const int N, const int inputs[51][10], const int combination[10]);
 
 int main(void) {
@@ -20,63 +21,32 @@ int main(void) {
 			cin >> inputs[i][j];
 		}
 	}
-	//가능한 모든 타순 조합에 대해 Get_Answer함수 호출
+	//가능한 모든 타순 조합에 대해 계산
 	for (int i = 1; i < 10; i++)	combination[i] = 0;
 	combination[4] = 1;
 
-	for (int a = 1; a < 10; a++) {
-		if (combination[a] == 0) {
-			combination[a] = 2;
-			for (int b = 1; b < 10; b++) {
-				if (combination[b] == 0) {
-					combination[b] = 3;
-					for (int c = 1; c < 10; c++) {
-						if (combination[c] == 0) {
-							combination[c] = 4;
-							for (int d = 1; d < 10; d++) {
-								if (combination[d] == 0) {
-									combination[d] = 5;
-									for (int e = 1; e < 10; e++) {
-										if (combination[e] == 0) {
-											combination[e] = 6;
-											for (int f = 1; f < 10; f++) {
-												if (combination[f] == 0) {
-													combination[f] = 7;
-													for (int g = 1; g < 10; g++) {
-														if (combination[g] == 0) {
-															combination[g] = 8;
-															for (int h = 1; h < 10; h++) {
-																if (combination[h] == 0) {
-																	combination[h] = 9;
-																	Get_Answer(N, inputs, combination);
-																	combination[h] = 0;
-																}
-															}
-															combination[g] = 0;
-														}
-													}
-													combination[f] = 0;
-												}
-											}
-											combination[e] = 0;
-										}
-									}
-									combination[d] = 0;
-								}
-							}
-							combination[c] = 0;
-						}
-					}
-					combination[b] = 0;
-				}
-			}
-			combination[a] = 0;
-		}
-	}
+	Make_Combination(N, inputs, combination, 2);
 
 	cout << answer;
 
 	return 0;
+}
+//DFS를 통해 모든 타순 조합을 만들고 Get_Answer함수를 호출
+//N과 inputs는 조합이 완성 된 뒤 Get_Answer를 호출하기 위해 전달받는다
+//combination은 타순의 조합을 의미하고 index는 이번 호출에서 어느 선수의 자리를 정할지를 의미한다
+void Make_Combination(const int N, const int inputs[51][10], int combination[10], int index) {
+	if (index == 10) {
+		Get_Answer(N, inputs, combination);
+	}
+	else {
+		for (int i = 1; i < 10; i++) {
+			if (combination[i] == 0) {
+				combination[i] = index;
+				Make_Combination(N, inputs, combination, index + 1);
+				combination[i] = 0;
+			}
+		}
+	}
 }
 
 void Get_Answer(const int N, const int inputs[51][10], const int combination[10]) {
